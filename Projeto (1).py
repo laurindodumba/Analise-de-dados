94f94f72153356f3087e2f23e7286c9e5e544a83
LAURINDO Dumba <38964642+laurindodumba@users.noreply.github.com>
2021-07-14 22:17:47 -0300

    Add files via upload

---
+++ Projeto (1).py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[36]:
+
+
+pip install pandas 
+
+
+# In[40]:
+
+
+import pandas as pd
+import pandera as pa
+
+pip install pandera 
+# In[34]:
+
+
+dataframe = pd.read_csv('projeto ciencia de dados.txt',sep=";")
+dataframe.head(20)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
